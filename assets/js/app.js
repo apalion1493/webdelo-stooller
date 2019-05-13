9e309cfdfee2519c6cd8208bd0769c20585f10be
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2019-05-13 10:36:43 +0300

    test1 13.05

--- assets/js/app.js
+++ assets/js/app.js
@@ -179,12 +179,12 @@ $(function() {
     //  the following simple make the textbox "Auto-Expand" as it is typed in
         .keyup(function(e) {
             //  this if statement checks to see if backspace or delete was pressed, if so, it resets the height of the box so it can be resized properly
-            if (e.which == 8 || e.which == 46) {
-                $(this).height(parseFloat($(this).css("min-height")) != 0 ? parseFloat($(this).css("min-height")) : parseFloat($(this).css("font-size")));
+            if (e.which === 8 || e.which === 46) {
+                $(this).height(parseFloat($(this).css("min-height")) !== 0 ? parseFloat($(this).css("min-height")) : parseFloat($(this).css("font-size")));
             }
             //  the following will help the text expand as typing takes place
             while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
                 $(this).height($(this).height()+1);
-            };
+            }
         });
 });
